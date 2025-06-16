154b3548c2e2844ca914d7611738096222e5d6b2
Searge <searge@duck.com>
2025-06-16 21:36:43 +0300

    :tada: Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "k8s-controller",
+	Short: "A production-grade Golang Kubernetes controller",
+	Long: `This project is a step-by-step tutorial for DevOps and SRE engineers
+	to learn about building Golang applications and Kubernetes controllers.
+	Each step is implemented as a feature branch and includes
+	a README section with explanations and command history
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.k8s-controller.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 Sergij Boremchuk searge@duck.com
+*/
+package main
+
+import "github.com/Searge/k8s-controller/cmd"
+
+func main() {
+	cmd.Execute()
+}
