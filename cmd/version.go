7fbb080876e895a9e687fbbe82c8aa07c4bf92c6
Searge <searge@duck.com>
2025-06-22 14:04:41 +0300

    feat(cmd): Enhance version implementatin

--- cmd/version.go
+++ cmd/version.go
@@ -9,9 +9,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// appVersion holds the current version of the application.
-// This value can be overridden at build time using ldflags.
-var appVersion = "dev"
+// Version holds the current version of the application.
+// This value can be overridden at build time using ldflags:
+// go build -ldflags "-X github.com/Searge/k8s-controller/cmd.Version=v1.0.0"
+var Version = "dev"
+
+// appVersion is kept for backward compatibility but now references Version
+var appVersion = Version
 
 // versionCmd represents the version command.
 // It displays the current version of the k8s-controller application
@@ -21,8 +25,8 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number",
 	Long:  `Print the version number of k8s-controller`,
 	Run: func(_ *cobra.Command, _ []string) {
-		log.Info().Str("version", appVersion).Msg("Version requested")
-		fmt.Printf("k8s-controller version %s\n", appVersion)
+		log.Info().Str("version", Version).Msg("Version requested")
+		fmt.Printf("k8s-controller version %s\n", Version)
 	},
 }
 
